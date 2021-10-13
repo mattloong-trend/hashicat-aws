79303c28c4a19bd07d6c1d018b49d954c08136ab
Your Name <you@example.com>
2021-10-13 04:14:56 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source  = "app.terraform.io/tf-workshop-matt/s3-bucket/aws"
   version = "2.2.0"
-  bucket = "${prefix}-my-s3-bucket"
+  bucket_prefix = "${var.prefix}"
   acl    = "private"
 
   versioning = {
